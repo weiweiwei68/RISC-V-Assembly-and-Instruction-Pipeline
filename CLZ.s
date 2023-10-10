bd72f689ae5a3c0a06a718b0fd8f4e750f9ed019
weiweiwei68 <136232155+weiweiwei68@users.noreply.github.com>
2023-10-10 21:12:26 +0800

    Add files via upload

--- CLZ.s
+++ CLZ.s
@@ -14,6 +14,12 @@ main:
     lw a0, num2
     lw a1, num1
     jal ra, count_leading_zeros
+    lw a0, num4
+    lw a1, num3
+    jal ra, count_leading_zeros
+    lw a0, num6
+    lw a1, num5
+    jal ra, count_leading_zeros
     
     #Exit program
     li a7, 10
@@ -56,10 +62,10 @@ count_ones: # Count ones (population count)
     and a2, a2, t0
     and a4, a4, t0
     # x = a1 a0 - a4 a2
-    slt a5, a0, a2           # a5 = 1 when a0 < a2
+    bge a2, a0, sub           # a5 = 1 when a0 < a2
     sub a0, a0, a2
     sub a1, a1, a4
-    sub a1, a1, a5
+    #sub a1, a1, a5
     
     # x = ((x >> 2) & 0x3333333333333333) + (x & 0x3333333333333333)
     slli a3, a1, 30     #put the shift value from x_high to correct position
@@ -135,4 +141,87 @@ count_ones: # Count ones (population count)
     lw ra, 16(sp)
     addi sp, sp, 24
     ret
-        
+sub:
+    addi a1, a1, -1
+    li a7, 0xffffffff
+    sub a7, a7, a2
+    addi a7, a7, 1
+    add a0, a7, a0
+    sub a1, a1, a4
+
+
+    
+    # x = ((x >> 2) & 0x3333333333333333) + (x & 0x3333333333333333)
+    slli a3, a1, 30     #put the shift value from x_high to correct position
+    srli a2, a0, 2           # shift right 2 bits in x_low
+    or a2, a2, a3
+    srli a4, a1, 2           #shift right 2 bits in x_high
+    li a7, 0x33333333
+    add t0, x0, a7
+    and a2, a2, t0       # lower 32 bits of ((x >> 2) & 0x3333333333333333)
+    and a4, a4, t0       # higher 32 bits of ((x >> 2) & 0x3333333333333333)
+    and a0, a0, t0       # lower 32 bits of (x & 0x3333333333333333)
+    and a1, a1, t0       # higher 32 bits of (x & 0x3333333333333333)
+    # x = a4 a2 + a1 a0
+    add a0, a0, a2
+    slt a5, a0, a2           # a5 = 1 when a0 < a2
+    add a1, a1, a4
+    add a1, a1, a5
+    
+    # x = ((x >> 4) + x) & 0x0f0f0f0f0f0f0f0f
+    slli a3, a1, 28     #put the shift value from x_high to correct position
+    srli a2, a0, 4      # shift right 4 bits in x_low
+    or a2, a2, a3
+    srli a4, a1, 4      # shift right 4 bits in x_high
+    # add a4 a2 and a1 a0. ((x >> 4) + x)
+    add a0, a0, a2
+    slt a5, a0, a2      # a5 = 1 when a0 < a2
+    add a1, a1, a4
+    add a1, a1, a5
+    # a1 a0 & 0x0f0f0f0f0f0f0f0f
+    li a7, 0x0f0f0f0f
+    add t0, x0, a7
+    and a0, a0, t0
+    and a1, a1, t0
+    
+    # x += (x >> 8)
+    slli a3, a1, 24     #put the shift value from x_high to correct position
+    srli a2, a0, 8      # shift right 4 bits in x_low
+    or a2, a2, a3
+    srli a4, a1, 8      #shift right 4 bits in x_high
+    # x = a1 a0 + a4 a2
+    add a0, a0, a2
+    slt a5, a0, a2      # a5 = 1 when a0 < a2
+    add a1, a1, a4
+    add a1, a1, a5
+    
+    # x += (x >> 16)
+    slli a3, a1, 16     #put the shift value from x_high to correct position
+    srli a2, a0, 16     # shift right 4 bits in x_low
+    or a2, a2, a3
+    srli a4, a1, 16     #shift right 4 bits in x_high
+    # x = a1 a0 + a4 a2
+    add a0, a0, a2
+    slt a5, a0, a2      # a5 = 1 when a0 < a2
+    add a1, a1, a4
+    add a1, a1, a5
+    
+    # x += (x >> 32)
+    slli a3, a1, 0
+    li a7, 32      #put the shift value from x_high to correct position
+    srl a2, a0, a7 
+    or a2, a2, a3
+    srl a4, a1, s7 
+    # x = a1 a0 + a4 a2
+    add a0, a0, a2
+    slt a5, a0, a2      # a5 = 1 when a0 < a2
+    add a1, a1, a4
+    add a1, a1, a5
+    
+    # (64 - (x & 0x7F))
+    addi t0, x0, 63
+    andi a0, a0, 0x7f
+    sub a0, t0, a0      # a0 represent the number of leading zeros
+    lw ra, 16(sp)
+    addi sp, sp, 24
+    ret
